3d9cfabd005cb66a01f9d8f8a0f6b4ef4fbfe5b4
Airika <erikamz2010@hotmail.com>
2013-09-15 22:40:29 -0700

    Start of an example Rails app, to show how to make a Tweet Queue.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RoRWorkshop::Application.config.secret_key_base = '125badb8c6c970229de549d8ec96cd300983ce5b5cf135b33e38b2aee4436fb7fcb48d435c50598b9e47febda92f7321f7f3a3637bec6811ed08245c7449c469'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RoRWorkshop::Application.config.session_store :cookie_store, key: '_RoR-Workshop_session'
